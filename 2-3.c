38cebb41d5c23cd31e592d9f385d9db39a45044c
sekine104 <sekine104@outlook.jp>
2020-09-25 16:12:44 +0900

    a

--- 2-3.c
+++ 2-3.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
 int main(void){
-    int a, b;
+  int a, b;
 
-    
-    return 0;
+  printf("Please enter two integers\n");
+  printf("Integer A:");
+  scanf("%d", &a);
+  printf("Integer B:");
+  scanf("%d", &b);
+  if(a!=b){
+    if(a>b){
+      printf("The larger value is %d\n", a);
+      printf("The smaller value is %d\n", b);
+    } else {
+      printf("The larger value is %d\n", b);
+      printf("The smaller value is %d\n", a);
+    }
+  } else {
+      printf("Both are %d\n", a);
+  }
+
+  return 0;
 }
