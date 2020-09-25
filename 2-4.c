73bf362fb265918d240887594868cc534efd1ab1
sekine104 <sekine104@outlook.jp>
2020-09-25 16:23:18 +0900

    a

--- 2-4.c
+++ 2-4.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(void){
+  int value;
+  int max, min;
 
-    return 0;
+  printf("Please enter three integers\n");
+  for(int i=1;i<4;i++){
+    printf("Integer (No.%d):", i);
+    scanf("%d", &value);
+    if(i==1){
+      max = value;
+      min = value;
+    } else if(value<min){
+      min = value;
+    } else if(value>max){
+      max = value;
+    }
+  }
+  printf("Maximum:%d\n", max);
+  printf("Minimum:%d\n", min);
+
+  return 0;
 }
