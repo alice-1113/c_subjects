2c9a615fd170b6b8aea4918b17dd51975c351334
sekine104 <sekine104@outlook.jp>
2020-10-02 17:36:17 +0900

    100

--- lesson/第3回/3-5.c
+++ lesson/第3回/3-5.c
@@ -3,7 +3,7 @@
 int main(void){
     int n, value;
     int num_max, num_min;
-    int max, min, sum;
+    int max, min, sum=0;
     float average;
 
     printf("Input the number of students: ");
@@ -12,20 +12,16 @@ int main(void){
         printf("No.%d: ", i);
         scanf("%d", &value);
         if(i==1||value<min){
-            if(value<min){
-                num_min = i;
-            }
+            num_min = i;
             min = value;
         }
         if(i==1||max<value){
-            if(max<value){
-                num_max = i;
-            }
+            num_max = i;
             max = value;
         }
         sum += value;
     }
-    average = (float)sum/ (float)n;
+    average = (double)sum/ (double)n;
     printf("Average: %.1f\n", average);
     printf("Max: No.%d, %d\n", num_max, max);
     printf("Min: No.%d, %d\n", num_min, min);
