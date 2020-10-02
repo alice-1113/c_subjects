929f04a42fd5f0ff2ab517a372b048ea4db6a3f5
sekine104 <sekine104@outlook.jp>
2020-10-02 17:10:40 +0900

    a

--- lesson/第3回/3-6.c
+++ lesson/第3回/3-6.c
@@ -3,21 +3,23 @@
 int main(void){
     int h,w;
 
-    printf("H: ");
-    scanf("%d", &h);
-    printf("W: ");
-    scanf("%d", &w);
+    do {
+        printf("H: ");
+        scanf("%d", &h);
+        printf("W: ");
+        scanf("%d", &w);
 
-    for(int y=1; y<=h; y++){
-        for(int x=1; x<=w; x++){
-            if((y==1||y==h)||(x==1||x==w)){
-                printf("#");
-            } else {
-                printf(".");
+        for(int y=1; y<=h; y++){
+            for(int x=1; x<=w; x++){
+                if((y==1||y==h)||(x==1||x==w)){
+                    printf("#");
+                } else {
+                    printf(".");
+                }
             }
+            printf("\n");
         }
-        printf("\n");
-    }
-    scanf("%d", &h);
+    } while (h!=0&&w!=0);
+
     return 0;
 }
