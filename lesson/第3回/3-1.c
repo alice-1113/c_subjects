2d598b3d4b8f896d171e6029d003eb84229fc209
sekine104 <sekine104@outlook.jp>
2020-10-02 16:13:01 +0900

    a

--- lesson/第3回/3-1.c
+++ lesson/第3回/3-1.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void){
-
+int main(void) {
+    int i;
+    i = 1;
+    while (i<=10) {
+        printf("i = %d\n", i);
+        i++;
+    }
+    scanf("%d", &i);
     return 0;
 }
