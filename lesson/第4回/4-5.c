3528f6a108fb72b02e1ff5ced7793582065920df
sekine104 <sekine104@outlook.jp>
2020-10-09 15:25:43 +0900

    a

--- lesson/第4回/4-5.c
+++ lesson/第4回/4-5.c
@@ -7,6 +7,7 @@ int main(void){
     int x1, y1;
     int x2, y2;
     double kyori;
+
     printf("<< Point A >>\n");
     printf("x coordinate value : ");
     scanf("%d", &x1);
@@ -18,6 +19,7 @@ int main(void){
     scanf("%d", &x2);
     printf("y coordinate value : ");
     scanf("%d", &y2);
+
     kyori = distance(x1, y1, x2, y2);
     printf("The distance between two points is %lf\n", kyori);
     return 0;
@@ -25,7 +27,6 @@ int main(void){
 
 double distance(int x1, int y1, int x2, int y2){
     double kyori;
-    // kyoriは仮
     double x = (double)(x2-x1);
     double y = (double)(y2-y1);
     kyori = sqrt(pow(x, 2.0) + pow(y, 2.0));
