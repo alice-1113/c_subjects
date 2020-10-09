4165488ec0c14130500e3bfeb0c28ea3005d492a
sekine104 <sekine104@outlook.jp>
2020-10-09 15:36:13 +0900

    a

--- lesson/第4回/4-2.c
+++ lesson/第4回/4-2.c
@@ -3,6 +3,19 @@
 int error_message();
 
 int main(void){
+    int en;
 
+    printf("Input an even number : ");
+    scanf("%d", &en);
+    if(en%2==0){
+        printf("The input value %d is even\n", en);
+    } else {
+        error_message();
+    }
     return 0;
+}
+
+int error_message(){
+    prinf("ERROR: invalid value!");
+    return 0
 }
