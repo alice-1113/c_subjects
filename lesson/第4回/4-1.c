cd2f8d90a1c92ad12109651a81125867b7664d07
sekine104 <sekine104@outlook.jp>
2020-10-09 17:03:19 +0900

    a

--- lesson/第4回/4-1.c
+++ lesson/第4回/4-1.c
@@ -9,18 +9,18 @@ int main(void){
     do{
         printf("base of triangle : ");
         scanf("%lf", &base);
-        if(base<1){
+        if(base<=0){
             printf("ERROR: value is not positive!\n");
         }
-    } while(base<1);
+    } while(base<=0);
 
     do{
         printf("hight of triangle : ");
         scanf("%lf", &hight);
-        if(hight<1){
+        if(hight<=0){
             printf("ERROR: value is not positive!\n");
         }
-    } while(hight<1);
+    } while(hight<=0);
 
     area = area_triangle(base, hight);
     printf("Area = %.2lf\n", area);
