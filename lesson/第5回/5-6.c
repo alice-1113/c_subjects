5e5568d2905698fee99532b2901e9c1c37c5c334
sekine104 <sekine104@outlook.jp>
2020-10-16 16:46:53 +0900

    a

--- lesson/第5回/5-6.c
+++ lesson/第5回/5-6.c
@@ -5,10 +5,59 @@ int max_value(int n);
 int min_value(int n);
 
 int main(void){
+    int value; // input number
+    int maxVal, minVal;
+
+    do {
+        // valueが1以上10以下でない場合、再入力
+        printf("Input number of data : ");
+        scanf("%d", &value);
+        if(value<1||10<value){
+            printf("ERROR : invalid value!\n\n");
+        }
+    } while(value<1||10<value);
+
+    for(int i=1;i<value+1;i++){
+        int num;
+        printf("\nNo. %d : ", i);
+        scanf("%d", &num);
+        maxVal = max_value(num);
+        minVal = min_value(num);
+        printf("RANGE : [ %d, %d ]\n\n", minVal, maxVal);
+    }
+
+    printf("max = %d\n", maxVal);
+    printf("min = %d\n", minVal);
 
     return 0;
 }
 
-int max_value(int n);
+int max_value(int n){
+    static int LocallyMaxVal;
+    static int count=0; // 呼び出し回数
 
-int min_value(int n);i
+    if(count==0){
+        LocallyMaxVal = n;
+    } else {
+        if(n>LocallyMaxVal){
+            LocallyMaxVal = n;
+        }
+    }
+    count++;
+    return LocallyMaxVal;
+}
+
+int min_value(int n){
+    static int LocallyMinVal;
+    static int count=0; // 呼び出し回数
+
+    if(count==0){
+        LocallyMinVal = n;
+    } else {
+        if(n<LocallyMinVal){
+            LocallyMinVal = n;
+        }
+    }
+    count++;
+    return LocallyMinVal;
+}
