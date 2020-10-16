43085905a86cec7fc6a70ed79172feebf340c8c2
sekine104 <sekine104@outlook.jp>
2020-10-16 13:55:08 +0900

    a

---
+++ convert.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+if sys.argv[1:]:
+    for argv in sys.argv[1:]:
+        if argv.endswith(".c"):
+            os.system(f"cd {os.path.dirname(argv)}")
+            os.system(f"gcc -Wall -o {argv.removesuffix('.c')} {argv}")
