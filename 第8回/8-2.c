19d0390b5f25a6c757c43060777cff3523d21027
sekine104 <sekine104@outlook.jp>
2020-11-12 01:42:09 +0900

    a

--- 第8回/8-2.c
+++ 第8回/8-2.c
@@ -5,7 +5,7 @@ void add_data(int array[], int n);
 
 int main(void){
     int array[SIZE];
-    int n, sum=0;
+    int n;
     printf("Enter the number of data　(1-10) :");
     scanf("%d", &n);
     printf("Enter data\n");
@@ -16,14 +16,14 @@ int main(void){
     add_data(array, n);
     printf("The computation results are listed as follows:\n");
     for(int i=0; i<n; i++){
-        sum += array[i];
-        printf("No.%d:%d\n", i+1, sum);
+        printf("No.%d:%d\n", i+1, array[i]);
     }
     return 0;
 }
 
 void add_data(int array[], int n){
-    for(int i=0; i<n; i++){
-        array[i] = array[i] * 10;
+    array[0] = array[0] * 10;
+    for(int i=1; i<n; i++){
+        array[i] = array[i]*10 + array[i-1];
     }
 }
